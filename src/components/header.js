95c680bd23d70d9386dbdaf0553cf282ce66aca3
Gaston82 <cabezaartaza@hotmail.com>
2021-11-11 11:13:36 +0100

    added reset component to the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import headerLogo from "../assets/images/rickandmorty-logo.png";
 import Form from "./form";
+import Reset from "./reset";
 
 export default function Header() {
   return (
@@ -8,6 +9,7 @@ export default function Header() {
       <div>
         <img className="header__logo" src={headerLogo} alt="Rick and Morty " />
         <Form />
+        <Reset />
       </div>
     </header>
   );
