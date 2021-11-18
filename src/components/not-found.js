ffb7d1b27de82f62466dbe8c0185fd158274f11c
Gaston82 <cabezaartaza@hotmail.com>
2021-11-18 23:31:23 +0100

    add css style to not found componenet

--- src/components/not-found.js
+++ src/components/not-found.js
@@ -1,9 +1,17 @@
 import notFoundImage from "../assets/images/not-found-rick-and-morty.jpg";
+import styles from "../styles/NotFound.module.css";
+
 export default function NotFound() {
   return (
     <>
-      <div className="container">
-        <img className="not-found-image" src={notFoundImage} alt="" />
+      <div className={styles.container}>
+        <img
+          className={styles.notFoundImage}
+          src={notFoundImage}
+          alt="Rick and Morty angry"
+        />
+        <h2 className={styles.notFoundMessage}>Character Not Found</h2>
+        <h2 className={styles.notFoundMessage}>Try again!!!</h2>
       </div>
     </>
   );
