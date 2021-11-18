e94c3055b75a0a075d01250a6cc3fa4a1ab86db2
Gaston82 <cabezaartaza@hotmail.com>
2021-11-18 06:19:42 +0100

    added detail card styles

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,6 +1,5 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
-import Character from "../components/character";
 import Header from "../components/header";
 import useCharacter from "../hooks/useCharacter";
 import styles from "../styles/Detail.module.css";
@@ -9,14 +8,31 @@ export function Detail() {
   let { id } = useParams();
   const { character } = useCharacter(id);
 
+  if (!character) {
+    return <p>Loading...</p>;
+  }
+  const { name, status, species, image, episode, location } = character;
   return (
     <>
       <Header />
       <div className={styles.detail__container}>
-        <Character {...character} />
-        <Link to={"/"}>
+        <article className={styles.card}>
+          <img className={styles.card__image} src={image} alt={name} />
+          <div className={styles.card__body}>
+            <h2 className={styles.card__title}>{name}</h2>
+            <div className={styles.card__header}>
+              <p>{status}-</p>
+              <p>{species}</p>
+            </div>
+            <p className={styles.card__location}>Last known location:</p>
+            <p className={styles.card__name__location}>{location.name}</p>
+            <p className={styles.card__location}>Total episodes:</p>
+            <p>{episode.length}</p>
+          </div>
+        </article>
+        {/* <Link to={"/"}>
           <p>Go Back to the list</p>
-        </Link>
+        </Link> */}
       </div>
     </>
   );
