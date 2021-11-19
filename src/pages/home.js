ddea2fcc9c8eff6a5367c1bfa96e7a1897b95040
Gaston82 <cabezaartaza@hotmail.com>
2021-11-19 01:12:04 +0100

    add Footer component to the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,4 @@
+import Footer from "../components/footer";
 import Form from "../components/form";
 import Header from "../components/header";
 import ListOfCharacters from "../components/listOfCharacters";
@@ -11,6 +12,7 @@ export function Home() {
         <Form />
       </div>
       <ListOfCharacters />
+      <Footer />
     </>
   );
 }
