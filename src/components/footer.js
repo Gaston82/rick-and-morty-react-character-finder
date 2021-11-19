903e47be598a8607da8a2ca92f5b0ff655feb84f
Gaston82 <cabezaartaza@hotmail.com>
2021-11-19 01:08:53 +0100

    add styles to Footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,16 @@
+import logo from "../assets/images/logo192.png";
+import styles from "../styles/Footer.module.css";
+
 export default function Footer() {
-  return <p>Footer component</p>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.flex}>
+          <p>Made in </p>
+          <img className={styles.logo} src={logo} alt=" React logo" />
+          <p>By Gastón</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
